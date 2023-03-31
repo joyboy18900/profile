25a9033dcfe44d7bf3b07d19e970b85fcc2427d2
joyboy18900 <surakiat.dev@gmail.com>
2023-03-31 17:45:14 +0700

    add more link about me more

--- src/components/MyPaginate.vue
+++ src/components/MyPaginate.vue
@@ -1,3 +1,62 @@
+<template>
+  <div class="flex items-center justify-between border-t border-dashed py-4 dark:border-night-600">
+    <!-- Small screen -->
+    <div class="flex flex-1 justify-between sm:hidden">
+      <button :disabled="isInFirstPage" @click="prevPage()" class="arrow-button rounded-lg">
+        <span class="sr-only">Previous</span>
+        <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
+        <span class="pr-2 text-xs"> {{ $t('paginate.previous_page') }} </span>
+      </button>
+      <button @click="nextPage()" :disabled="isInLastPage" class="arrow-button rounded-lg">
+        <span class="sr-only">Next</span>
+        <span class="pl-2 text-xs"> {{ $t('paginate.next_page') }} </span>
+        <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
+      </button>
+    </div>
+
+    <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+      <div>
+        <p class="text-sm text-gray-700 dark:text-night-300">
+          {{ $t('paginate.count', { first: firstItem + 1, last: lastItem, total: totalItems }) }}
+        </p>
+      </div>
+      <div>
+        <nav class="isolate inline-flex gap-2 -space-x-px" aria-label="Pagination">
+          <button :disabled="isInFirstPage" @click="firstPage()" class="arrow-button rounded-full">
+            <span class="sr-only">{{ $t('paginate.first_page') }}</span>
+            <ChevronDoubleLeftIcon class="h-5 w-5" aria-hidden="true" />
+          </button>
+          <button :disabled="isInFirstPage" @click="prevPage()" class="arrow-button rounded-lg">
+            <span class="sr-only">{{ $t('paginate.previous_page') }}</span>
+            <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
+          </button>
+
+          <button
+            v-for="(page, index) in pages"
+            :key="index"
+            class="button"
+            type="button"
+            @click="selectPage(page.id)"
+            :disabled="page.isDisabled"
+            :class="{ active: isPageActive(page.id) }"
+            :aria-label="`Go to page number ${page.id}`">
+            {{ page.id }}
+          </button>
+
+          <button @click="nextPage()" :disabled="isInLastPage" class="arrow-button rounded-lg">
+            <span class="sr-only">{{ $t('paginate.next_page') }}</span>
+            <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
+          </button>
+          <button :disabled="isInLastPage" @click="lastPage()" class="arrow-button rounded-full">
+            <span class="sr-only">{{ $t('paginate.last_page') }}</span>
+            <ChevronDoubleRightIcon class="h-5 w-5" aria-hidden="true" />
+          </button>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { computed, defineEmits, ref } from 'vue';
 import {
@@ -101,65 +160,6 @@ const lastItem = computed(() => {
   return props.totalItems;
 });
 </script>
-
-<template>
-  <div class="flex items-center justify-between border-t border-dashed py-4 dark:border-night-600">
-    <!-- Small screen -->
-    <div class="flex flex-1 justify-between sm:hidden">
-      <button :disabled="isInFirstPage" @click="prevPage()" class="arrow-button rounded-lg">
-        <span class="sr-only">Previous</span>
-        <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
-        <span class="pr-2 text-xs"> {{ $t('paginate.previous_page') }} </span>
-      </button>
-      <button @click="nextPage()" :disabled="isInLastPage" class="arrow-button rounded-lg">
-        <span class="sr-only">Next</span>
-        <span class="pl-2 text-xs"> {{ $t('paginate.next_page') }} </span>
-        <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
-      </button>
-    </div>
-
-    <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-      <div>
-        <p class="text-sm text-gray-700 dark:text-night-300">
-          {{ $t('paginate.count', { first: firstItem + 1, last: lastItem, total: totalItems }) }}
-        </p>
-      </div>
-      <div>
-        <nav class="isolate inline-flex gap-2 -space-x-px" aria-label="Pagination">
-          <button :disabled="isInFirstPage" @click="firstPage()" class="arrow-button rounded-full">
-            <span class="sr-only">{{ $t('paginate.first_page') }}</span>
-            <ChevronDoubleLeftIcon class="h-5 w-5" aria-hidden="true" />
-          </button>
-          <button :disabled="isInFirstPage" @click="prevPage()" class="arrow-button rounded-lg">
-            <span class="sr-only">{{ $t('paginate.previous_page') }}</span>
-            <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
-          </button>
-
-          <button
-            v-for="(page, index) in pages"
-            :key="index"
-            class="button"
-            type="button"
-            @click="selectPage(page.id)"
-            :disabled="page.isDisabled"
-            :class="{ active: isPageActive(page.id) }"
-            :aria-label="`Go to page number ${page.id}`">
-            {{ page.id }}
-          </button>
-
-          <button @click="nextPage()" :disabled="isInLastPage" class="arrow-button rounded-lg">
-            <span class="sr-only">{{ $t('paginate.next_page') }}</span>
-            <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
-          </button>
-          <button :disabled="isInLastPage" @click="lastPage()" class="arrow-button rounded-full">
-            <span class="sr-only">{{ $t('paginate.last_page') }}</span>
-            <ChevronDoubleRightIcon class="h-5 w-5" aria-hidden="true" />
-          </button>
-        </nav>
-      </div>
-    </div>
-  </div>
-</template>
 
 <style>
 .button {
